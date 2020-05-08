241b7597811fd0db86be0529a231bd07083ebbe2
whiterkim <whiterkim@gmail.com>
2020-05-08 16:29:46 -0700

    adjust nav bar format, fix genderless display bug

--- src/app/pipe/gender-rate.pipe.ts
+++ src/app/pipe/gender-rate.pipe.ts
@@ -3,6 +3,10 @@ import { Pipe, PipeTransform } from '@angular/core';
 @Pipe({name: 'genderRate'})
 export class GenderRatePipe implements PipeTransform {
   transform(genderRate: number): string {
+    if (genderRate === -1) {
+      return "Genderless";
+    }
+
     let femaleRate = genderRate / 8.0 * 100;
     let maleRate = 100 - femaleRate;
     return "♂" + maleRate + "% ♀" + femaleRate + "%";
