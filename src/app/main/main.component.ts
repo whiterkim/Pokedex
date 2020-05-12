e7866b56a756124dff413eb7bc341cb24f1d8ff3
Whiter Kim <whiterkim@gmail.com>
2020-05-11 18:01:37 -0700

    add loading page

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { ApiService } from '../service/api.service';
 
 @Component({
   selector: 'app-main',
@@ -7,9 +10,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private router: Router,
+    private apiService: ApiService,
+  ) { }
 
   ngOnInit() {
+    this.load();
+  }
+
+  async load() {
+    let baseUrl = "https://pokeapi.co/api/v2/";
+    for (let i = 1; i <= 5; i++) {
+      await this.apiService.get(baseUrl + 'item/' + i.toString());
+    }
+    this.router.navigate(['/pokemon-list']);
+  }
+
+  async sleep(ms: number) {
+    return new Promise(resolve => setTimeout(resolve, ms));
   }
 
 }
